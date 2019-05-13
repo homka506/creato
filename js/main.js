ae26a03723adb79f3b962699196464a3025315d6
unknown <polina.kalashnikova1985@gmail.com>
2019-05-13 12:53:58 +0300

    all

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 ;(function() {
 	"use strict";
-
+let btn = document.querySelector('.ba-menu-toggle');
+		let menu = document.querySelector('.ba-menu');
+		btn.addEventListener('click', function (e) {
+			menu.classList.toggle('open');
+		});
 })();
+
 
